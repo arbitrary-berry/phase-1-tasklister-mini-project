95d5c7c2a7af5e77dd475011eb39b4e912e0b53e
Alyssa <alyssa.brooksong@gmail.com>
2023-06-21 10:23:47 -0700

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+document.querySelector("form").addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault()
+  const input = document.getElementById('new-task-description').value
+  console.log(input)
+  const list = document.querySelector('ul')
+  const item = document.createElement('li')
+  item.textContent = input
+  list.append(item)
+}
